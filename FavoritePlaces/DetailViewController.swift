0feb3a5938bc25872e92f0750aea9d1820598473
Felix Barros <felix.barros@gmail.com>
2015-12-18 20:11:27 -0500

    Change status bar in detail view to be light

--- FavoritePlaces/DetailViewController.swift
+++ FavoritePlaces/DetailViewController.swift
@@ -15,7 +15,7 @@ class DetailViewController: UIViewController {
 
         // Do any additional setup after loading the view.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -25,14 +25,8 @@ class DetailViewController: UIViewController {
         dismissViewControllerAnimated(true, completion: nil)
     }
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    override func preferredStatusBarStyle() -> UIStatusBarStyle {
+        return UIStatusBarStyle.LightContent
     }
-    */
 
 }
